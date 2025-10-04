89c27d0fcc3e305cd1b6d5868d60bec3a8d83ab3
Circuit City <zerowrkay@gmail.com>
2025-10-04 17:21:40 +0800

    Update Javascript.js

--- resources/js/Javascript.js
+++ resources/js/Javascript.js
@@ -3,11 +3,12 @@ const params = new URLSearchParams(window.location.search);
 const from = params.get('from');
 
 // Determine where to go back
-let backUrl = 'file:///E:/PRMLANG/HTML/RWD.html'; // default fallback
+let backUrl = '../html/RWD.html'; // default fallback
 if (from === 'second-main') {
-  backUrl = 'file:///E:/PRMLANG/HTML/RGD.html';
+  backUrl = '../html/RGD.html';
 }
 
 document.getElementById('exit').addEventListener('click', function() {
   window.location.href = backUrl;
+
 });
